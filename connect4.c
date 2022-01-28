e438fc1370a60c523283f1f134f1f0e5d3c5af81
Robert Preus-MacLaren <rob@neontribe.co.uk>
2022-01-28 12:34:25 +0000

    fix winning algo

--- connect4.c
+++ connect4.c
@@ -12,20 +12,36 @@
 #define HEIGHT 6
 #define WIDTH 7
 
-char board[HEIGHT][WIDTH] = {
-    {' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+enum BoardState {
+    STATE_Empty,
+    STATE_Player1,
+    STATE_Player2,
+};
+int board[HEIGHT][WIDTH] = {
+    {STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty},
+    {STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty},
+    {STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty},
+    {STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty},
+    {STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty},
+    {STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty, STATE_Empty},
 };
 
 int col = 0;
 int colsize = 2;
-char player = 'o';
+char player = STATE_Player1;
 int winner = -1;
 bool program_should_exit = false;
+
+char mapState(enum BoardState bs) {
+    switch(bs) {
+        case STATE_Empty:
+            return '_';
+        case STATE_Player1:
+            return 'o';
+        case STATE_Player2:
+            return 'x';
+    }
+}
 
 void renderBoard() {
     // system("clear");
@@ -43,14 +59,14 @@ void renderBoard() {
     for (int i = 0; i < col * colsize; i++) {
         printf(" ");
     }
-    printf("%c\n", player);
+    printf("%c\n", mapState(player));
 
     // ascii art
     printf("%s_______________\n", prefix);
     for (int i = 0; i < HEIGHT; i++) {
         printf("%s|", prefix);
         for (int j = 0; j < WIDTH; j++) {
-            printf("%c|", board[i][j] == ' ' ? '_' : board[i][j]);
+            printf("%c|", mapState(board[i][j]));
         }
         printf("\n");
     }
@@ -60,32 +76,31 @@ void renderBoard() {
 
 void moveRight() { if (col < WIDTH - 1) { col++; } }
 void moveLeft() { if (col > 0) { col--; } }
-void togglePlayer() { if (player == 'o') player = 'x'; else player = 'o';}
+void togglePlayer() { if (player == STATE_Player1) player = STATE_Player2; else player = STATE_Player1;}
 
-void placePiece() {
-    for (int i = HEIGHT; i >= 0; i--) {
+int placePiece() {
+    int placedY = -1;
+    for (int i = HEIGHT - 1; i >= 0; i--)
+    {
         char target = board[i][col];
 
-        if (target == ' ') {
+        if (target == STATE_Empty) {
             board[i][col] = player;
+            placedY = i;
             break;
         }
     }
 
-    togglePlayer();
+    return placedY;
 }
 
-void checkWinner(int placedPieceX) {
-    int placedPieceY = 0;
-    for (int i = 0; i < HEIGHT; i++) {
-        char check = board[i][placedPieceX];
-
-        if (check != ' ') {
-            placedPieceY = i;
-            break;
-        }
+void printArr(char arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%c", arr[i]);
     }
+}
 
+void checkWinner(int x, int y) {
     char row[WIDTH];
     char col[HEIGHT];
     char forwardDiag[HEIGHT];
@@ -93,39 +108,32 @@ void checkWinner(int placedPieceX) {
 
     // ROW
     for (int i = 0; i < WIDTH; i++) {
-        row[i] = board[placedPieceY][i];
+        row[i] = mapState(board[y][i]);
     }
     // col
-    for (int i = 0; i < HEIGHT; i++) {
-        printf("col '%s'\n", col);
-        col[i] = board[i][placedPieceX];
+    for (int j = 0; j < HEIGHT; j++) {
+        col[j] = mapState(board[j][x]);
     }
 
-    printf("looking for '%s' in '%s'\n", "xxxx", row);
-    printf("looking for '%s' in '%s'\n", "xxxx", col);
+    printf("row: ");
+    printArr(row, WIDTH);
+    printf("\t looking for contiguos values\n");
+
+    printf("col: ");
+    printArr(col, HEIGHT);
+    printf("\t looking for contiguos values\n");
+
     if (strstr(row, "oooo") || strstr(col, "oooo")){
-        winner = 1;
+        winner = STATE_Player1;
     }
     if (strstr(row, "xxxx") || strstr(col, "xxxx")){
-        winner = 1;
+        winner = STATE_Player2;
     }
 }
 
-//        V
-//        x
-//   _______________
-//   |_|_|_|_|_|_|_|
-//   |_|_|_|_|_|_|_|
-//   |_|_|_|x|x|_|_|
-//   |_|_|_|x|o|o|_|
-//   |_|_|x|o|x|o|_|
-//   |_|x|o|o|o|x|_|
-//  /|=============|\
-// /-|-------------|-\
-
 int main() {
     int loops = 0;
-    int c, oldCol;
+    int c, oldCol, placedRow;
 
     while (!program_should_exit) {
         renderBoard();
@@ -149,8 +157,12 @@ int main() {
             break;
         case KEY_Space:
             oldCol = col;
-            placePiece();
-            checkWinner(oldCol);
+            placedRow = placePiece();
+
+            if (placedRow != -1) {
+                togglePlayer();
+                checkWinner(oldCol, placedRow);
+            }
             break;
         default:
             if (loops++ >= 10) {
@@ -159,4 +171,5 @@ int main() {
             break;
         }
     }
+    printf("EXITING\n");
 }
